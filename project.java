c1d84fc9692eca1beef8982919b4cd20157c3402
NIRANJAN NAMBIAR <niranjannambiar6@gmail.com>
2026-01-31 15:27:24 +0530

    Add files via upload

---
+++ project.java
@@ -0,0 +1,23 @@
+public class Main {
+
+    // Method to add two numbers
+    public static int addNumbers(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to my Java GitHub project!");
+
+        int num1 = 10;
+        int num2 = 20;
+
+        int result = addNumbers(num1, num2);
+
+        System.out.println("First number: " + num1);
+        System.out.println("Second number: " + num2);
+        System.out.println("Sum: " + result);
+
+        System.out.println("Program finished successfully.");
+    }
+}
+
